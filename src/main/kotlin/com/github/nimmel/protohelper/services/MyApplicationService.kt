6953558ba797d2674d31d50d6185d901dba1684e
GitHub Action <action@github.com>
2020-09-03 11:41:57 +0000

    Template cleanup

--- src/main/kotlin/com/github/nimmel/protohelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nimmel/protohelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nimmel.protohelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nimmel.protohelper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/nimmel/protohelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nimmel/protohelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nimmel.protohelper.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nimmel.protohelper.MyBundle
 
 class MyProjectService(project: Project) {
 
